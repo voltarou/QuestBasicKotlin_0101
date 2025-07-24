34fddb070003ecaf5a163c0a2317e36ea3ddea20
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:48:29 +0700

    data 2

--- Tugas 1/DataClass.kt
+++ Tugas 1/DataClass.kt
@@ -9,3 +9,20 @@ data class DataClass(
     val id: Int,
     var email: String
 )
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+
+    // Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+    // Fungsi Copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
