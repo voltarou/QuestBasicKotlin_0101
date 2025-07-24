52c049870c74fdc7d066a6f15200de91969d0008
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:47:22 +0700

    conditional 2

--- Tugas 1/ConditionalExpression.kt
+++ Tugas 1/ConditionalExpression.kt
@@ -14,3 +14,59 @@ fun ConditionalStatement() {
     } else {
         println("Negative Number")
     }
+
+    val day = 3
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    // For Loop
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car Type: $car")
+    }
+
+    // While Loop
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    // Do-While Loop
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
+}
