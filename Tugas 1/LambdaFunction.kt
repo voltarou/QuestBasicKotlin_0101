2734233e611ef451a4b174e0c71e95c22d06a88c
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:51:21 +0700

    Lambda 2

--- Tugas 1/LambdaFunction.kt
+++ Tugas 1/LambdaFunction.kt
@@ -4,3 +4,14 @@ package com.example.test
 // Lambda functions adalah fungsi yang tidak memiliki nama
 // Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
 // Lambda functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun upperCaseString(string: String): String {
+    return string.uppercase()
+}
+
+//Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    upperCaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+    // HELLO
+}
