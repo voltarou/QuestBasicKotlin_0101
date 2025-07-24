6ef3203b3c89c0c81e9e496aab777d115ca5040e
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:50:56 +0700

    Lambda

---
+++ Tugas 1/LambdaFunction.kt
@@ -0,0 +1,6 @@
+package com.example.test
+
+// Lambda Functions
+// Lambda functions adalah fungsi yang tidak memiliki nama
+// Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
