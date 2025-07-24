b588059965350d3cec0272f180d9088d7c9e7b4a
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:46:51 +0700

    conditional1

---
+++ Tugas 1/ConditionalExpression.kt
@@ -0,0 +1,16 @@
+package com.example.test
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Kotlin mendukung conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
