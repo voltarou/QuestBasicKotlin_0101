9d8ba3945cab5daa3e9cd637d068a2253c4ea8c2
voltarou <161274983+voltarou@users.noreply.github.com>
2025-07-24 18:50:04 +0700

    functions 2

--- Tugas 1/Functions.kt
+++ Tugas 1/Functions.kt
@@ -4,3 +4,40 @@ fun withoutParameter() {
     println("=== withoutParameter ===")
     println("Hello, World!")
 }
+
+fun withParameter(name: String) {
+    println()
+    println("=== withParameter ===")
+    println("Hello, $name!")
+}
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("=== withNamedArgument ===")
+    println("Hello, $name! You are $age years old.")
+}
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("=== withDefaultParameter ===")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25) // Example of using default for 'name'
+    withDefaultParameter(name = "Alice", age = 28) // Example of overriding default 'name'
+    
+    val area = withReturn(10, 5)
+    println("\nArea: $area")
+}
